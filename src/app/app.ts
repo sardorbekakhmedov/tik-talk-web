10a3a4a7bc684957ba7fa5020e35df56521bbc63
sardorbek_a <sardorbe2015@gmail.com>
2026-08-24 18:43:09 +0500

    Enhance profile card component: added dynamic profile data input, conditional rendering, new styles, and improved integration in App module.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
 import { ProfileCard } from "./common-ui/profile-card/profile-card";
+import { ProfileService } from './data/services/profile.service';
+import { JsonPipe } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCard],
+  imports: [ProfileCard, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('tik-talk');
+
+  profileService = inject(ProfileService)
+  profiles: Profile[] = [];
+
+  constructor() {
+   this.profileService.getTestAccounts()
+    .subscribe(val => {
+      this.profiles = val
+   });
+  }
 }
--- src/app/common-ui/profile-card/profile-card.ts
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-profile-card',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
-export class ProfileCard {}
+export class ProfileCard {
+  @Input() profile!: Profile;
+
+
+
+}
